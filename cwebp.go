c8ebf1475c8d38ef2666951e39149a79d0441188
gyula <isnt@gyula.cool>
2021-02-17 01:08:44 +0100

    yet again

--- cwebp.go
+++ cwebp.go
@@ -35,8 +35,7 @@ func NewCWebP() *CWebP {
 		BinWrapper: createBinWrapper(),
 		quality:    -1,
 	}
-	fmt.Println("c")
-	bin.ExecPath("docker run -it --rm -v /usr/local:/data elswork/cwebp cwebp")
+	bin.ExecPath("cwebp")
 
 	return bin
 }
